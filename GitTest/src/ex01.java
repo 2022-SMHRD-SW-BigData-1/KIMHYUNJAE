2c4f201f33c0e046b21e077d1ba2c5465be5c3f8
EGEG22A06 <EGEG22A06@DESKTOP-V59O6TA>
2022-09-02 09:15:35 +0900

    vcb

--- GitTest/src/ex01.java
+++ GitTest/src/ex01.java
@@ -5,7 +5,11 @@ public class ex01 {
 		
 		System.out.println("첫번쨰 커밋!");
 		System.out.println("두번쨰 커밋!");
-		System.out.println("세번째!");
+		// 방법
+		// src에서 오른쪽 마우스 팀 commit ->오른쪽에 commit 실행
+		// 다시 team ->push Branch master 클릭 -> 로그인(이메일,토큰값입력)
+		// 깃허브 확인
+		System.out.println("세번째!!!");
 		
 	}
 
